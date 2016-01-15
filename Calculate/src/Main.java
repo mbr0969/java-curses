9adc92a382cb6f94fcbbde9477d2a022ee41c1a5
papa <mbr0969@gmail.com>
2016-01-15 23:22:15 +0300

    Change 2 Calculate

--- Calculate/src/Main.java
+++ Calculate/src/Main.java
@@ -10,5 +10,22 @@ public class Main {
         int summ = first + second;
 
         System.out.println("Сумма " + args[0] + " и " + args[1]+ " равна " + summ);
+
+        int razn = first - second;
+        System.out.println("Разность " + args[0] + " и " + args[1]+ " равна " + razn);
+
+        int proiz = first * second;
+        System.out.println("Произведение " + args[0] + " и " + args[1]+ " равно " + proiz);
+
+        double a = Double.valueOf(args[0]);
+        double b = Double.valueOf(args[1]);
+        double delen = a / b;
+        System.out.println("Деление " + args[0] + " на " + args[1]+ " равно " + delen);
+
+        int step = (int) Math.pow(first, second);
+        System.out.println("Cтепень  числа " + args[0] + " показатель степени  " + args[1]+ " равно " + step);
+
+
+
     }
 }
