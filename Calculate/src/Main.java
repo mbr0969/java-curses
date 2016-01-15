49ddb35c15be03de7dc02e1db8ead691ad9261ef
papa <mbr0969@gmail.com>
2016-01-15 23:07:38 +0300

    Change 1 Calculate

--- Calculate/src/Main.java
+++ Calculate/src/Main.java
@@ -4,6 +4,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Calulate...");
-        System.out.println("Calulate...");
+
+        int first = Integer.valueOf(args[0]);
+        int second = Integer.valueOf(args[1]);
+        int summ = first + second;
+
+        System.out.println("Сумма " + args[0] + " и " + args[1]+ " равна " + summ);
     }
 }
