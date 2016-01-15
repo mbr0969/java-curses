c249e136495f0602e0cd047e743350b4453b0213
papa <mbr0969@gmail.com>
2016-01-15 22:50:03 +0300

    Create Calculate

---
+++ Calculate/src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by papa on 15.01.16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Calulate...");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+/**
+ * Created by papa on 15.01.16.
+ */
+public class Main {
+}
