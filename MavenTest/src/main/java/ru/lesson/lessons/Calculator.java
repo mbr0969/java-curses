50173ebecc8fabb4f0ccc3da248c093145896b92
mbr0969 <mbr0969@gmail.com>
2016-09-25 17:42:45 +0300

    17:41

--- MavenTest/src/main/java/ru/lesson/lessons/Calculator.java
+++ MavenTest/src/main/java/ru/lesson/lessons/Calculator.java
@@ -3,34 +3,36 @@ package ru.lesson.lessons;
 
 public class Calculator {
 
-//Результат вычеслений
-     private int result;
+    //Результат вычеслений
+    private int result;
 //Суммируем аргументы
 
-    public void add(int ... params)
-    {
-        for (Integer param : params ){
+    public void add(int... params) {
+        for (Integer param : params) {
             this.result += param;
-      }
-    }
-public void div(int... args ){
-    if (args.length  > 0){
-
-    }else{
-
-    }
-}
-
-//получаем результат
-    public int getResult(){
-      return this.result;
+        }
     }
 
-//очищаем рузальтат
-    public void cleanRezult(){
-      this.result = 0;
+    public int div(int first, int second) {
+          if (second != 0) {
+                return this.result = first / second;
+              } else {
+                System.out.println("Division by zero");
+            }
+        return 1;
+        }
+
+
+
+    //получаем результат
+    public int getResult() {
+        return this.result;
     }
 
+    //очищаем рузальтат
+    public void cleanRezult() {
+        this.result = 0;
+    }
 
 
 }
