864a66ead724937e64d19e0b954a81ca25c4f91e
mbr0969 <mbr0969@gmail.com>
2016-09-25 17:22:26 +0300

    17:22

--- MavenTest/src/test/java/ru/lesson/lessons/CalculatorTest.java
+++ MavenTest/src/test/java/ru/lesson/lessons/CalculatorTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
-
+    
     @Test
     public void testAdd() throws Exception {
         Calculator calculator = new Calculator();
