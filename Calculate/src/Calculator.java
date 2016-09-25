779c1d30d149e29ea30565bb645f561d2fefdacf
mbr0969 <mbr0969@gmail.com>
2016-09-25 05:02:58 +0300

    creat Calculate summ

--- Calculate/src/Calculator.java
+++ Calculate/src/Calculator.java
@@ -27,6 +27,15 @@ public class Calculator {
 	
 	public static void main(String[] args){
 		System.out.println("Calcalate");
+		int first = Integer.valueOf(args[0]);
+		int second = Integer.valueOf(args[1]);
+		int summ = first + second;
+		System.out.println("Summa: " + summ);
+		summ = first * second;
+		System.out.println("Proisved: " + summ);
+		summ = first/second;	
+		System.out.println("Delenie: "+ summ);
+
 	}
 
 }
