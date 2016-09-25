4779689d549bea3e38611c0484c7aed5ef0e490b
mbr0969 <mbr0969@gmail.com>
2016-09-25 17:01:35 +0300

    25.09.16 17:27

---
+++ MavenTest/src/main/java/ru/lesson/lessons/Calculate.java
@@ -0,0 +1,7 @@
+package ru.lesson.lessons;
+
+/**
+ * Created by papa on 25.09.2016.
+ */
+public class Calculate {
+}
---
+++ MavenTest/src/test/java/ru/lesson/lessons/CalculatorTest.java
@@ -0,0 +1,16 @@
+package ru.lesson.lessons;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by papa on 25.09.2016.
+ */
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() throws Exception {
+
+    }
+}
